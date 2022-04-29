091648f6ee57ff4afab8c21939ffe019aff46d45
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-04-29 23:21:42 +0900

    add csrfToken to frontend

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { csrfToken } from '@rails/ujs';
 import App from '../components/App';
+import axios from 'axios';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = createRoot(document.getElementById('root'));
   root.render(<App />);
 });
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken();
