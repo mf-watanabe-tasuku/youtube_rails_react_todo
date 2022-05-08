a8aa29e7d4e831b3d012aae165eebea4d80f7e40
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-05-08 10:32:41 +0900

    rename CSS file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -6,7 +6,7 @@ import TodoList from './TodoList';
 import EditTodo from './EditTodo';
 import Categories from './CategoryList';
 import EditCategory from './EditCategory';
-import './App.css';
+import '../styles/application.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
